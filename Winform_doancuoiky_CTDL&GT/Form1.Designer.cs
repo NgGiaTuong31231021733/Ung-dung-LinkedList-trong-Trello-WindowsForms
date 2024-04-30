104e9807d9aa46dc6ad05bcb40eb5923c99dc5e3
HP <HP@LAPTOP-7DMKHKLP>
2024-04-30 09:17:14 +0700

    Add project files.

---
+++ Winform_doancuoiky_CTDL&GT/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Winform_doancuoiky_CTDL_GT
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.toDoList = new System.Windows.Forms.ListView();
+            this.addTaskToDoButton = new System.Windows.Forms.Button();
+            this.inProgressList = new System.Windows.Forms.ListView();
+            this.addTaskInProgressButton = new System.Windows.Forms.Button();
+            this.doneList = new System.Windows.Forms.ListView();
+            this.addTaskDoneButton = new System.Windows.Forms.Button();
+            this.removeTaskButton = new System.Windows.Forms.Button();
+            this.addListButton = new System.Windows.Forms.Button();
+            this.labelToDo = new System.Windows.Forms.Label();
+            this.labelInProgress = new System.Windows.Forms.Label();
+            this.labelDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // toDoList
+            // 
+            this.toDoList.HideSelection = false;
+            this.toDoList.Location = new System.Drawing.Point(16, 15);
+            this.toDoList.Margin = new System.Windows.Forms.Padding(4);
+            this.toDoList.Name = "toDoList";
+            this.toDoList.Size = new System.Drawing.Size(265, 245);
+            this.toDoList.TabIndex = 0;
+            this.toDoList.UseCompatibleStateImageBehavior = false;
+            // 
+            // addTaskToDoButton
+            // 
+            this.addTaskToDoButton.Location = new System.Drawing.Point(16, 268);
+            this.addTaskToDoButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addTaskToDoButton.Name = "addTaskToDoButton";
+            this.addTaskToDoButton.Size = new System.Drawing.Size(265, 28);
+            this.addTaskToDoButton.TabIndex = 1;
+            this.addTaskToDoButton.Tag = this.toDoList;
+            this.addTaskToDoButton.Text = "Add Card";
+            this.addTaskToDoButton.UseVisualStyleBackColor = true;
+            this.addTaskToDoButton.Click += new System.EventHandler(this.addTaskToDoButton_Click);
+            // 
+            // inProgressList
+            // 
+            this.inProgressList.HideSelection = false;
+            this.inProgressList.Location = new System.Drawing.Point(291, 15);
+            this.inProgressList.Margin = new System.Windows.Forms.Padding(4);
+            this.inProgressList.Name = "inProgressList";
+            this.inProgressList.Size = new System.Drawing.Size(265, 245);
+            this.inProgressList.TabIndex = 2;
+            this.inProgressList.UseCompatibleStateImageBehavior = false;
+            // 
+            // addTaskInProgressButton
+            // 
+            this.addTaskInProgressButton.Location = new System.Drawing.Point(291, 268);
+            this.addTaskInProgressButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addTaskInProgressButton.Name = "addTaskInProgressButton";
+            this.addTaskInProgressButton.Size = new System.Drawing.Size(265, 28);
+            this.addTaskInProgressButton.TabIndex = 3;
+            this.addTaskInProgressButton.Tag = this.inProgressList;
+            this.addTaskInProgressButton.Text = "Add Card";
+            this.addTaskInProgressButton.UseVisualStyleBackColor = true;
+            this.addTaskInProgressButton.Click += new System.EventHandler(this.addTaskInProgressButton_Click);
+            // 
+            // doneList
+            // 
+            this.doneList.HideSelection = false;
+            this.doneList.Location = new System.Drawing.Point(565, 15);
+            this.doneList.Margin = new System.Windows.Forms.Padding(4);
+            this.doneList.Name = "doneList";
+            this.doneList.Size = new System.Drawing.Size(265, 245);
+            this.doneList.TabIndex = 4;
+            this.doneList.UseCompatibleStateImageBehavior = false;
+            // 
+            // addTaskDoneButton
+            // 
+            this.addTaskDoneButton.Location = new System.Drawing.Point(565, 268);
+            this.addTaskDoneButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addTaskDoneButton.Name = "addTaskDoneButton";
+            this.addTaskDoneButton.Size = new System.Drawing.Size(265, 28);
+            this.addTaskDoneButton.TabIndex = 5;
+            this.addTaskDoneButton.Tag = this.doneList;
+            this.addTaskDoneButton.Text = "Add Card";
+            this.addTaskDoneButton.UseVisualStyleBackColor = true;
+            this.addTaskDoneButton.Click += new System.EventHandler(this.addTaskDoneButton_Click);
+            // 
+            // removeTaskButton
+            // 
+            this.removeTaskButton.Location = new System.Drawing.Point(16, 336);
+            this.removeTaskButton.Margin = new System.Windows.Forms.Padding(4);
+            this.removeTaskButton.Name = "removeTaskButton";
+            this.removeTaskButton.Size = new System.Drawing.Size(133, 28);
+            this.removeTaskButton.TabIndex = 7;
+            this.removeTaskButton.Text = "Remove Card";
+            this.removeTaskButton.UseVisualStyleBackColor = true;
+            this.removeTaskButton.Click += new System.EventHandler(this.removeTaskButton_Click);
+            // 
+            // addListButton
+            // 
+            this.addListButton.Location = new System.Drawing.Point(226, 336);
+            this.addListButton.Name = "addListButton";
+            this.addListButton.Size = new System.Drawing.Size(133, 28);
+            this.addListButton.TabIndex = 0;
+            this.addListButton.Text = "Add List";
+            this.addListButton.UseVisualStyleBackColor = true;
+            this.addListButton.Click += new System.EventHandler(this.addListButton_Click);
+            // 
+            // labelToDo
+            // 
+            this.labelToDo.AutoSize = true;
+            this.labelToDo.Location = new System.Drawing.Point(16, 0);
+            this.labelToDo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelToDo.Name = "labelToDo";
+            this.labelToDo.Size = new System.Drawing.Size(45, 16);
+            this.labelToDo.TabIndex = 0;
+            this.labelToDo.Text = "To Do";
+            // 
+            // labelInProgress
+            // 
+            this.labelInProgress.AutoSize = true;
+            this.labelInProgress.Location = new System.Drawing.Point(290, 0);
+            this.labelInProgress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInProgress.Name = "labelInProgress";
+            this.labelInProgress.Size = new System.Drawing.Size(75, 16);
+            this.labelInProgress.TabIndex = 1;
+            this.labelInProgress.Text = "In Progress";
+            // 
+            // labelDone
+            // 
+            this.labelDone.AutoSize = true;
+            this.labelDone.Location = new System.Drawing.Point(566, 0);
+            this.labelDone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDone.Name = "labelDone";
+            this.labelDone.Size = new System.Drawing.Size(40, 16);
+            this.labelDone.TabIndex = 2;
+            this.labelDone.Text = "Done";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 412);
+            this.Controls.Add(this.labelToDo);
+            this.Controls.Add(this.labelInProgress);
+            this.Controls.Add(this.labelDone);
+            this.Controls.Add(this.addListButton);
+            this.Controls.Add(this.removeTaskButton);
+            this.Controls.Add(this.addTaskDoneButton);
+            this.Controls.Add(this.doneList);
+            this.Controls.Add(this.addTaskInProgressButton);
+            this.Controls.Add(this.inProgressList);
+            this.Controls.Add(this.addTaskToDoButton);
+            this.Controls.Add(this.toDoList);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Trello Board";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ListView toDoList;
+        private System.Windows.Forms.Button addTaskToDoButton;
+        private System.Windows.Forms.ListView inProgressList;
+        private System.Windows.Forms.Button addTaskInProgressButton;
+        private System.Windows.Forms.ListView doneList;
+        private System.Windows.Forms.Button addTaskDoneButton;
+        private System.Windows.Forms.Button removeTaskButton;
+        private System.Windows.Forms.Button addListButton;
+        private Label labelToDo;
+        private Label labelInProgress;
+        private Label labelDone;
+    }
+}
---
+++ Winform_doancuoiky_CTDL&GT/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Winform_doancuoiky_CTDL_GT
+{
+    public partial class Form1 : Form
+    {
+        private LinkedList<ListView> lists;
+        private LinkedList<ListViewItem> selectedItems;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeLists();
+            selectedItems = new LinkedList<ListViewItem>();
+        }
+
+        private void InitializeLists()
+        {
+            lists = new LinkedList<ListView>();
+            lists.AddLast(toDoList);
+            lists.AddLast(inProgressList);
+            lists.AddLast(doneList);
+
+            foreach (ListView list in lists)
+            {
+                list.MultiSelect = false;
+                list.View = View.List;
+                list.Click += List_Click;
+                list.AllowDrop = true;
+                list.ItemDrag += List_ItemDrag;
+                list.DragEnter += List_DragEnter;
+                list.DragDrop += List_DragDrop;
+            }
+        }
+
+
+        private void List_Click(object sender, EventArgs e)
+        {
+            ListView listView = sender as ListView;
+            if (listView.SelectedItems.Count > 0)
+            {
+                selectedItems.AddLast(listView.SelectedItems[0]);
+            }
+        }
+
+        private string GetTaskNameFromUserInput(string prompt, string title)
+        {
+            return Interaction.InputBox(prompt, title);
+        }
+
+        private void addTaskToDoButton_Click(object sender, EventArgs e)
+        {
+            ListView listView = (sender as Button).Tag as ListView;
+
+            string taskName = GetTaskNameFromUserInput("Enter Task Name", "Add Card to ToDo");
+            if (!string.IsNullOrEmpty(taskName))
+            {
+                listView.Items.Add(taskName);
+            }
+        }
+
+        private void addTaskInProgressButton_Click(object sender, EventArgs e)
+        {
+            ListView listView = (sender as Button).Tag as ListView;
+
+            string taskName = GetTaskNameFromUserInput("Enter Task Name", "Add Card to InProgress");
+            if (!string.IsNullOrEmpty(taskName))
+            {
+                listView.Items.Add(taskName);
+            }
+        }
+
+        private void addTaskDoneButton_Click(object sender, EventArgs e)
+        {
+            ListView listView = (sender as Button).Tag as ListView;
+
+            string taskName = GetTaskNameFromUserInput("Enter Task Name", "Add Card to Done");
+            if (!string.IsNullOrEmpty(taskName))
+            {
+                listView.Items.Add(taskName);
+            }
+        }
+
+
+        private void addListButton_Click(object sender, EventArgs e)
+        {
+            string newListName = Interaction.InputBox("Enter List Name", "Add New List");
+            if (!string.IsNullOrEmpty(newListName))
+            {
+                ListView newList = new ListView();
+                newList.HideSelection = false;
+
+                newList.Tag = newListName;
+
+                newList.Location = new System.Drawing.Point(this.Controls.Cast<Control>().Max(c => c.Right) + 6, doneList.Location.Y);
+
+                newList.Margin = new System.Windows.Forms.Padding(4);
+                newList.Name = "newList" + (lists.Count + 1).ToString();
+                newList.Size = doneList.Size;
+                newList.TabIndex = 4;
+                newList.UseCompatibleStateImageBehavior = false;
+                newList.View = View.List;
+
+                newList.Click += List_Click;
+                newList.AllowDrop = true;
+                newList.ItemDrag += List_ItemDrag;
+                newList.DragEnter += List_DragEnter;
+                newList.DragDrop += List_DragDrop;
+
+                this.Controls.Add(newList);
+                lists.AddLast(newList);
+
+                Button addTaskToListButton = new Button();
+                addTaskToListButton.Text = "Add Card";
+                addTaskToListButton.Location = new System.Drawing.Point(newList.Location.X, newList.Location.Y + newList.Height + 6);
+                addTaskToListButton.Size = new System.Drawing.Size(202, 23);
+                addTaskToListButton.Margin = new System.Windows.Forms.Padding(4);
+                addTaskToListButton.Tag = newList;
+                addTaskToListButton.Click += AddTaskToListButton_Click;
+                this.Controls.Add(addTaskToListButton);
+
+                Label newLabel = new Label();
+                newLabel.AutoSize = true;
+                newLabel.Location = new System.Drawing.Point(newList.Location.X, 0);
+                newLabel.Name = "label" + newList.Name;
+                newLabel.Size = new System.Drawing.Size(25, 16);
+                newLabel.TabIndex = 2;
+                newLabel.Text = newListName;
+                this.Controls.Add(newLabel);
+
+                this.Height += newList.Height;
+
+
+            }
+        }
+
+        private void AddTaskToListButton_Click(object sender, EventArgs e)
+        {
+            ListView listView = (sender as Button).Tag as ListView;
+
+            string currentNewListName = listView.Tag.ToString();
+
+            string newTask = Interaction.InputBox("Enter Task Name", "Add Card to" + " " + currentNewListName);
+            if (!string.IsNullOrEmpty(newTask))
+            {
+                listView.Items.Add(newTask);
+            }
+        }
+
+        private void List_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            ListView list = sender as ListView;
+            list.DoDragDrop(e.Item, DragDropEffects.Move);
+        }
+
+        private void List_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ListViewItem)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void List_DragDrop(object sender, DragEventArgs e)
+        {
+            ListView destinationList = sender as ListView;
+            ListViewItem draggedItem = e.Data.GetData(typeof(ListViewItem)) as ListViewItem;
+            ListView sourceList = draggedItem.ListView;
+
+            if (sourceList != destinationList)
+            {
+                draggedItem.Remove();
+                destinationList.Items.Add(draggedItem);
+            }
+        }
+
+
+
+        private void removeTaskButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem selectedItem in selectedItems)
+            {
+                selectedItem.Remove();
+            }
+            selectedItems.Clear();
+        }
+    }
+}
+
+
